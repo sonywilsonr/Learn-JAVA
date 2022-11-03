61b86c9a3aa1d8e9236f106df943b6c8ddb191a8
Sony R <117156437+sonywilsonr@users.noreply.github.com>
2022-11-03 17:59:47 +0530

    Create Hello.java

---
+++ ClassSample/Hello.java
@@ -0,0 +1,18 @@
+package classample;
+
+public class Helloo {
+public static void main(String arg[]) {
+	Samplee s1=new Samplee();
+	Samplee s2=new Samplee();
+	s1.num1=100;
+	s2.num1=200;
+	s1.num2=150;
+	s2.num2=250;
+	
+	s1.sum();
+	s2.sum();
+	s1.display();
+	s2.display();
+	
+}
+}
