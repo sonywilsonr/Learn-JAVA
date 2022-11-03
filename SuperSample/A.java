97e770c5731d58ca1bdb0821cd3081b58cf98445
Sony R <117156437+sonywilsonr@users.noreply.github.com>
2022-11-03 17:35:21 +0530

    Create A.java

---
+++ SuperSample/A.java
@@ -0,0 +1,13 @@
+public class A {
+int a;
+
+void display() {
+	System.out.println("its A");
+} 
+A(){
+	System.out.println("its A constructor");
+}
+A(int a){
+	System.out.println("its argument constructor");
+}
+}
